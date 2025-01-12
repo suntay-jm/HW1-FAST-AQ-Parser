78c320b1f0aaffb689ef47e9cd546407e4bac52d
suntay-jm <98995579+suntay-jm@users.noreply.github.com>
2025-01-11 17:22:16 -0800

    Update seq.py
    filled in the functions with the code that (reverse) transcribes

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -1,22 +1,23 @@
 # DNA -> RNA Transcription
-from typing import Union
 
 TRANSCRIPTION_MAPPING = {"A": "U", "C": "G", "T": "A", "G": "C"}
-ALLOWED_NUC = TRANSCRIPTION_MAPPING.keys()
 
-
-def transcribe(seq: str, reverse: bool = False) -> str:
+def transcribe(dna_string):
     """
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    transcribed = ""
+    for nt in dna_string:
+        transcribed += TRANSCRIPTION_MAPPING[nt]
+    return transcribed
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    rev_comp = ""
+    for nt in dna_string[::-1]:
+        rev_comp += TRANSCRIPTION_MAPPING[nt]
+    return rev_comp
