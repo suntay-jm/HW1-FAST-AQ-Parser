791ff241d9992fceab0ee8871a760fa90c94bd2f
Matt Suntay <suntay.jm@gmail.com>
2025-01-15 20:05:34 -0800

    trigger github actions workflow

--- .ipynb_checkpoints/example-checkpoint.py
+++ .ipynb_checkpoints/example-checkpoint.py
@@ -9,20 +9,42 @@ def main():
     The main function
     """
     # Create instance of FastaParser
+    fasta = FastaParser("data/test.fa")
+    
     # Create instance of FastqParser
+    fastq = FastqParser("data/test.fq")
         
     # For each record of FastaParser, Transcribe the sequence
     # and print it to console
+    for header, seq in fasta:
+        transcribed_seq = transcribe(seq)
+        print(f">{header}\n{transcribed_seq}")
+
+    print("====== switching to fastq ======")
        
     # For each record of FastqParser, Transcribe the sequence
     # and print it to console
+    for header, seq, quality in fastq:
+        transcribed_seq = transcribe(seq)
+        print(f"@{header}\n{transcribed_seq}")
 
+    print("====== switching to reverse fasta ======")
 
     # For each record of FastaParser, Reverse Transcribe the sequence
     # and print it to console
-       
+
+    for header, seq in fasta:
+        reverse_transcribed_seq = reverse_transcribe(seq)
+        print(f">{header}_reversed\n{reverse_transcribed_seq}")
+
+    print("====== switching to reverse fastq ======")
+    
     # For each record of FastqParser, Reverse Transcribe the sequence
     # and print it to console
+    
+    for header, seq, quality in fastq:
+        reverse_transcribed_seq = reverse_transcribe(seq)
+        print(f"@{header}_reversed\n{reverse_transcribed_seq}")
 
 
 """
